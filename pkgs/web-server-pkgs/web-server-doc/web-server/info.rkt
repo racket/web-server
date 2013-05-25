c0142533c9804a6654aca8ad69d02c156e5cff5d
Matthew Flatt <mflatt@racket-lang.org>
2013-05-24 21:57:31 -0600

    reorganize into core plus packages
    The "racket" directory contains a pared-back version of the
    repository, roughly.
    
    The "pkgs" directory everything else in the repository, but
    organized into packages.
    
    original commit: b2ebb0a28bf8136e75cd98316c22fe54c30eacb2

--- pkgs/web-server-pkgs/web-server-doc/web-server/info.rkt
+++ pkgs/web-server-pkgs/web-server-doc/web-server/info.rkt
@@ -5,8 +5,4 @@
     ("scribblings/web-server-internal.scrbl" (multi-page) (tool))
     ("scribblings/tutorial/continue.scrbl" () (tutorial 5))))
 
-(define mzscheme-launcher-libraries '("main.rkt"))
-(define mzscheme-launcher-names     '("PLT Web Server"))
-
-(define compile-omit-paths '("default-web-root"
-                             "scribblings/tutorial/examples"))
+(define compile-omit-paths '("scribblings/tutorial/examples"))
