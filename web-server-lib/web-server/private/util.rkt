8f7db2257c822fd672332be47425b17193367a4c
Vincent St-Amour <stamourv@racket-lang.org>
2015-07-17 14:16:28 -0500

    Remove dependency on unstable/bytes.
    ... by moving the functionality back to the web-server utils.

--- web-server-lib/web-server/private/util.rkt
+++ web-server-lib/web-server/private/util.rkt
@@ -5,11 +5,9 @@
          unstable/contract
          racket/serialize
          net/url-structs)
-(require unstable/bytes
-         unstable/contract)
+(require unstable/contract)
 (provide
  (all-from-out
-  unstable/bytes
   unstable/contract)
  list-prefix?)
 
@@ -89,6 +87,30 @@
  [path-without-base (path-string? path-string? . -> . (listof path-piece?))]
  [directory-part (path-string? . -> . path?)]
  [build-path-unless-absolute (path-string? path-string? . -> . path?)])
+
+;; --
+
+(define (read/bytes bs)
+  (read (open-input-bytes bs)))
+;; Eli: This is a really bad name for something that is often called
+;;   `read-from-string', or `read-from-bytes' in this case.  I first read it as
+;;   "read with bytes".  Regardless, I see little point in composing two
+;;   functions where the two names are clear enough -- you might consider
+;;   looking at the version in CL.
+;; Ryan: I agree. More useful would be a version that checked that the
+;; bytes contains only one S-expr and errors otherwise.
+
+(define (write/bytes v)
+  (define by (open-output-bytes))
+  (write v by)
+  (get-output-bytes by))
+;; Eli: Same bad name as above.  Also, is there any point in this given
+;;   (format "~s" v), and the fact that using the resulting string for printout
+;;   will get the same result.
+
+(provide/contract
+ [read/bytes (bytes? . -> . printable/c)]
+ [write/bytes (printable/c . -> . bytes?)])
 
 ;; --
 
