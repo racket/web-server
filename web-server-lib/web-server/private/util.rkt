3b5c620a171e2b087bcc87758fa5a85af00be060
Vincent St-Amour <stamourv@racket-lang.org>
2015-07-17 10:49:53 -0500

    Use exn->string from racket/exn.

--- web-server-lib/web-server/private/util.rkt
+++ web-server-lib/web-server/private/util.rkt
@@ -1,5 +1,6 @@
 #lang racket/base
 (require racket/contract/base
+         racket/exn
          racket/list
          unstable/contract
          racket/serialize
@@ -32,14 +33,6 @@
 (define (network-error src fmt . args)
   (raise (make-exn:fail:network (format "~a: ~a" src (apply format fmt args))
                                 (current-continuation-marks))))
-
-;; exn->string : (or/c exn any) -> string
-(define (exn->string exn)
-  (if (exn? exn)
-      (parameterize ([current-error-port (open-output-string)])
-        ((error-display-handler) (exn-message exn) exn)
-        (get-output-string (current-error-port)))
-      (format "~s\n" exn)))
 
 (provide/contract
  [network-error (->* [symbol? string?] [] #:rest list? void?)]
