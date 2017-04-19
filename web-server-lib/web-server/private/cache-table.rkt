66e72191ce89f9307857cd18bb93189d1ec57fd0
Kimball Germane <krgermane@gmail.com>
2017-04-19 02:00:38 +0000

    Allow flushed servlet instances to be specified.

--- web-server-lib/web-server/private/cache-table.rkt
+++ web-server-lib/web-server/private/cache-table.rkt
@@ -8,11 +8,15 @@
   (make-cache-table (make-hasheq)
                     (make-semaphore 1)))
 
-(define (cache-table-clear! ct)
+(define (cache-table-clear! ct [entry-ids #f])
   (call-with-semaphore
    (cache-table-semaphore ct)
    (lambda ()
-     (set-cache-table-hash! ct (make-hasheq)))))
+     (if entry-ids
+       (let ([cache-hash (cache-table-hash ct)])
+         (for ([entry-id (in-list entry-ids)])
+           (hash-remove! cache-hash entry-id))) 
+       (set-cache-table-hash! ct (make-hasheq))))))
 
 (define (cache-table-lookup! ct entry-id entry-thunk)
   (define ht (cache-table-hash ct))
@@ -31,5 +35,5 @@
  [rename new-cache-table make-cache-table
          (-> cache-table?)]
  [cache-table-lookup! (cache-table? symbol? (-> any/c) . -> . any/c)]
- [cache-table-clear! (cache-table? . -> . void?)]
+ [cache-table-clear! ((cache-table?) ((or/c false/c (listof symbol?))) . ->* . void?)]
  [cache-table? (any/c . -> . boolean?)])
