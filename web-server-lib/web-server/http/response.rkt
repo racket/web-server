87f1592b4940f1bdf46be3de22851604597137bc
Bogdan Popa <popa.bogdanp@gmail.com>
2018-11-10 16:13:47 +0200

    chore: fix extraneous whitespace

--- web-server-lib/web-server/http/response.rkt
+++ web-server-lib/web-server/http/response.rkt
@@ -22,7 +22,7 @@
 
 (define (output-response/method conn resp meth)
   (cond
-    [(or 
+    [(or
       ;; If it is terminated, just continue
       (terminated-response? resp)
       ;; If it is HTTP/1.0, ditto
@@ -63,7 +63,7 @@
    (append
     (maybe-headers
      seen?
-     [#"Date" 
+     [#"Date"
       (string->bytes/utf-8 (seconds->gmt-string (current-seconds)))]
      [#"Last-Modified"
       (string->bytes/utf-8 (seconds->gmt-string (response-seconds bresp)))]
@@ -98,7 +98,7 @@
 (define (terminated-response? r)
   (define hs (response-headers r))
   (or (headers-assq* #"Content-Length" hs)
-      (cond 
+      (cond
         [(headers-assq* #"Transfer-Encoding" hs)
          => (λ (h) (not (bytes=? (header-value h) #"identity")))]
         [else #f])))
@@ -224,7 +224,7 @@
                       conn
                       (make-416-response modified-seconds maybe-mime-type)))])
     (let* (; converted-ranges : (alist-of integer integer)
-           ; This is a list of actual start and end offsets in the file. 
+           ; This is a list of actual start and end offsets in the file.
            ; See the comments for convert-http-ranges for more information.
            [converted-ranges
             (if ranges
@@ -250,7 +250,7 @@
                            (bytes-length headers)      ; length of the headers and header newlinw
                            (- (cdr range) (car range)) ; length of the content
                            2))                         ; length of the content newline
-                      (+ (* (+ boundary-length 4) 
+                      (+ (* (+ boundary-length 4)
                             (length converted-ranges)) ; length of the intermediate boundaries
                          (+ boundary-length 6))        ; length of the final boundary
                       converted-ranges
@@ -278,7 +278,7 @@
                   ; Multiple ranges are encoded as multipart/byteranges:
                   (let loop ([ranges converted-ranges] [multipart-headers multipart-headers])
                     (match ranges
-                      [(list) 
+                      [(list)
                        ; Final boundary (must start on new line; ends with a new line)
                        (fprintf (connection-o-port conn) "--~a--\r\n" boundary)
                        (void)]
@@ -322,7 +322,7 @@
 ;; Converts a list of HTTP ranges:
 ;;
 ;; into pairs of offsets we can use to read from a file:
-;; 
+;;
 ;;   - suffix-byte-range-specs are converted to pairs of absolute offsets;
 ;;   - missing end offsets in byte-range-specs ranges are filled in;
 ;;   - end offsets are exclusive (as opposed to the inclusive offsets in ranges and the HTTP spec).
@@ -367,7 +367,7 @@
         (response
          206 #"Partial content"
          modified-seconds
-         maybe-mime-type 
+         maybe-mime-type
          (list (make-header #"Accept-Ranges" #"bytes")
                (make-content-length-header total-content-length)
                (make-content-range-header start end total-file-length))
@@ -385,7 +385,7 @@
   (response
    200 #"OK"
    modified-seconds
-   maybe-mime-type 
+   maybe-mime-type
    (list (make-header #"Accept-Ranges" #"bytes")
          (make-content-length-header total-content-length))
    void))
@@ -395,7 +395,7 @@
   (response
    416 #"Invalid range request"
    modified-seconds
-   maybe-mime-type 
+   maybe-mime-type
    null
    void))
 
@@ -406,7 +406,7 @@
 ;; make-content-range-header : integer integer integer -> header
 ;; start must be inclusive; end must be exclusive.
 (define (make-content-range-header start end total-file-length)
-  (make-header #"Content-Range" 
+  (make-header #"Content-Range"
                (string->bytes/utf-8
                 (format "bytes ~a-~a/~a" start (sub1 end) total-file-length))))
 
