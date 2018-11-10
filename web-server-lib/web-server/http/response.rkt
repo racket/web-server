b33f37e092f9e70312fca8b5aa28a065e9c4d2e9
Bogdan Popa <popa.bogdanp@gmail.com>
2018-11-10 16:16:56 +0200

    test: verify response chunker buffering behaviour
    These tests simulate how an OS socket might buffer the output data of a
    chunked request and verify that the output is flushed after each chunk
    correctly.

--- web-server-lib/web-server/http/response.rkt
+++ web-server-lib/web-server/http/response.rkt
@@ -130,6 +130,112 @@
   (fprintf to-client "\r\n")
   (flush-output to-client))
 
+(module+ test
+  (require rackunit)
+
+  (define (make-buffered-pipe #:bufsize [bufsize 4096])
+    (define-values [in out] (make-pipe))
+
+    (define position 0)
+    (define buffer (make-bytes bufsize))
+
+    (define (flush!)
+      (write-bytes-avail buffer out 0 position)
+      (set! position 0)
+      0)
+
+    (define (chomp! bs start-pos end-pos non-blocking? enable-breaks?)
+      (define input-size (- end-pos start-pos))
+
+      (define (go current-pos)
+        (define capacity (- bufsize position))
+        (define chunk-size (min capacity (- end-pos current-pos)))
+
+        (cond
+          [(= capacity 0)
+           (flush!)
+           (go current-pos)]
+
+          [(< current-pos end-pos)
+           (bytes-copy! buffer position bs current-pos (+ current-pos chunk-size))
+           (set! position (+ position chunk-size))
+           (go (+ current-pos chunk-size))]
+
+          [else input-size]))
+
+      (if (> input-size 0)
+          (go start-pos)
+          (flush!)))
+
+    (define (close)
+      (flush!)
+      (close-output-port out))
+
+    (values (make-input-port 'in in in (lambda () (close-input-port in)))
+            (make-output-port 'out out chomp! close)))
+
+  (define (call-with-test-client+server resp f)
+    (define-values [in out] (make-buffered-pipe))
+    (define chunks (make-channel))
+    (define client
+      (thread
+       (lambda ()
+         (define chunk (make-bytes 4096))
+         (let loop ()
+           (define n-read-or-eof (read-bytes-avail! chunk in))
+           (unless (eof-object? n-read-or-eof)
+             (channel-put chunks (subbytes chunk 0 n-read-or-eof))
+             (loop))))))
+
+    (define server
+      (thread
+       (lambda ()
+         (define connection
+           (new-connection (start-connection-manager) 120 (current-input-port) out (current-custodian) #f))
+         (output-response connection resp))))
+
+    (dynamic-wind
+      void
+      (lambda ()
+        (f in out chunks))
+      (lambda ()
+        (kill-thread client)
+        (kill-thread server))))
+
+  (define write-ready (make-semaphore))
+  (define resp
+    (response/output
+     (lambda (out)
+       (semaphore-wait write-ready)
+       (write-bytes #"a" out)
+       (semaphore-wait write-ready)
+       (write-bytes #"b" out)
+       (semaphore-wait write-ready))))
+
+  (call-with-test-client+server resp
+   (lambda (in out chunks)
+     (check-equal? (subbytes (sync/timeout 1 chunks) 0 8) #"HTTP/1.1")
+
+     (semaphore-post write-ready)
+     (check-equal? (sync/timeout 1 chunks) #"1\r\na\r\n")
+
+     (semaphore-post write-ready)
+     (check-equal? (sync/timeout 1 chunks) #"1\r\nb\r\n")
+
+     (semaphore-post write-ready)
+     (check-equal? (sync/timeout 1 chunks) #"0\r\n\r\n")))
+
+  (call-with-test-client+server resp
+   (lambda (in out chunks)
+     (check-equal? (subbytes (sync/timeout 1 chunks) 0 8) #"HTTP/1.1")
+
+     (semaphore-post write-ready)
+     (check-equal? (sync/timeout 1 chunks) #"1\r\na\r\n")
+     (close-output-port out)
+
+     (semaphore-post write-ready)
+     (check-equal? (sync/timeout 1 chunks) #f))))
+
 ; seconds->gmt-string : Nat -> String
 ; format is rfc1123 compliant according to rfc2068 (http/1.1)
 (define (seconds->gmt-string s)
