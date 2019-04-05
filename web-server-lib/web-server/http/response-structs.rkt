9ecca0461b790a8e51678364358d1d6b839c3344
Jesse Alama <jesse@lisp.sh>
2019-04-05 06:20:40 +0200

    Infer an HTTP response status message from a code
    Previously, a status message was required (with a default of
    "Okay"). Infer the status message now from the response
    code, using the status messages defined in RFCs 7235 and
    7231. "OK" (rather than "Okay") now serves as the fallback
    message if an unknown code is used (such as 499 or 123).

--- web-server-lib/web-server/http/response-structs.rkt
+++ web-server-lib/web-server/http/response-structs.rkt
@@ -1,14 +1,22 @@
 #lang racket/base
-(require racket/contract 
-         web-server/http/request-structs)
+(require racket/contract
+         racket/match
+         web-server/http/request-structs
+         "status-code.rkt")
+
+(module+ test
+  (require rackunit))
 
 (define TEXT/HTML-MIME-TYPE #"text/html; charset=utf-8")
 
 (struct response (code message seconds mime headers output))
 
 (define (response/full code message seconds mime headers body)
-  (response code message seconds mime
-            (list* (make-header #"Content-Length" 
+  (response code
+            (infer-response-message code message)
+            seconds
+            mime
+            (list* (make-header #"Content-Length"
                                 (string->bytes/utf-8
                                  (number->string
                                   (for/fold ([len 0])
@@ -21,13 +29,49 @@
 
 (define (response/output output
                          #:code [code 200]
-                         #:message [message #"Okay"]
+                         #:message [message #f]
                          #:seconds [seconds (current-seconds)]
                          #:mime-type [mime-type TEXT/HTML-MIME-TYPE]
                          #:headers [headers '()])
-  (response code message seconds mime-type headers
+  (response code
+            (infer-response-message code message)
+            seconds
+            mime-type
+            headers
             output))
 
+(module+ test
+  (let ([output (lambda (op) void)])
+    ;; check message as bytes
+    (let [(resp (response/output output
+                                 #:code 123
+                                 #:message #"bites!"))]
+      (check-equal? (response-code resp) 123)
+      (check-equal? (response-message resp) #"bites!"))
+    ;; check message as #f
+    (let [(resp (response/output output
+                                 #:code 200
+                                 #:message #f))]
+      (check-equal? (response-code resp) 200)
+      (check-equal? (response-message resp) #"OK"))
+    ;; check message not supplied, but code supplied
+    (let [(resp (response/output output
+                                 #:code 200))]
+      (check-equal? (response-code resp) 200)
+      (check-equal? (response-message resp) #"OK"))
+    ;; check code not supplied, message supplied
+    (let [(resp (response/output output
+                                 #:message #"bite this"))]
+      (check-equal? (response-code resp) 200)
+      (check-equal? (response-message resp) #"bite this"))
+    ;; check neither message nor code supplied
+    (let [(resp (response/output output))]
+      (check-equal? (response-code resp) 200)
+      (check-equal? (response-message resp) #"OK"))
+    ;; check non-standard status code
+    (let [(resp (response/output output #:code 123))]
+      (check-equal? (response-code resp) 123)
+      (check-equal? (response-message resp) #"OK"))))
 
 (provide/contract
  [struct response
@@ -37,10 +81,10 @@
           [mime (or/c false/c bytes?)]
           [headers (listof header?)]
           [output (output-port? . -> . any)])]
- [response/full (-> number? bytes? number? (or/c false/c bytes?) (listof header?) (listof bytes?) response?)]
+ [response/full (-> number? (or/c false/c bytes?) number? (or/c false/c bytes?) (listof header?) (listof bytes?) response?)]
  [response/output (->* ((-> output-port? any))
                        (#:code number?
-                        #:message bytes?
+                        #:message (or/c false/c bytes?)
                         #:seconds number?
                         #:mime-type (or/c bytes? #f)
                         #:headers (listof header?))
