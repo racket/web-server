5a52579d034c417583a00e0759515e7fb11b74fd
Jesse Alama <jesse@lisp.sh>
2019-07-29 07:58:49 +0200

    Use the new write-response utility for JSON tests

--- web-server-test/tests/web-server/http/json.rkt
+++ web-server-test/tests/web-server/http/json.rkt
@@ -11,34 +11,34 @@
 (define tm (start-timer-manager))
 
 (test
- (write-response (response/jsexpr '("booga")))
+ (write-response tm (response/jsexpr '("booga")))
  =>
  (bytes-sort
   #"HTTP/1.1 200 OK\r\nDate: REDACTED GMT\r\nLast-Modified: REDACTED GMT\r\nServer: Racket\r\nContent-Type: application/json; charset=utf-8\r\n\r\n[\"booga\"")
 
  ; Showing off all default headers and other values (200, "OK"):
- (write-response (response/jsexpr (hasheq 'jay "zee")))
+ (write-response tm (response/jsexpr (hasheq 'jay "zee")))
  =>
  (bytes-sort
   #"HTTP/1.1 200 OK\r\nDate: REDACTED GMT\r\nLast-Modified: REDACTED GMT\r\nServer: Racket\r\nContent-Type: text/html; charset=utf-8\r\n\r\n{\"jay\":\"zee\"}")
 
  ; We respect the json-null parameter:
- (write-response (parameterize ([json-null 'jibby-jab])
-                   (response/jsexpr 'jibby-jab)))
+ (write-response tm (parameterize ([json-null 'jibby-jab])
+                      (response/jsexpr 'jibby-jab)))
  =>
  (bytes-sort
   #"HTTP/1.1 200 OK\r\nDate: REDACTED GMT\r\nLast-Modified: REDACTED GMT\r\nServer: Racket\r\nContent-Type: text/html; charset=utf-8\r\n\r\nnull")
 
- (write-response (response/jsexpr "jazzy jazz"
-                                  #:code 404))
+ (write-response tm (response/jsexpr "jazzy jazz"
+                                     #:code 404))
  =>
  (bytes-sort
   #"HTTP/1.1 404 Not Found\r\nDate: REDACTED GMT\r\nLast-Modified: REDACTED GMT\r\nServer: Racket\r\nContent-Type: text/html; charset=utf-8\r\n\r\n\"jazzy jazz\"")
 
  ; The message is normally inferred from the status code,
  ; but you can use whatever you want:
- (write-response (response/jsexpr #f
-                                  #:message #"Strange request"))
+ (write-response tm (response/jsexpr #f
+                                     #:message #"Strange request"))
  =>
  (bytes-sort
   #"HTTP/1.1 200 Strange request\r\nDate: REDACTED GMT\r\nLast-Modified: REDACTED GMT\r\nServer: Racket\r\nContent-Type: text/html; charset=utf-8\r\n\r\nfalse")
@@ -48,8 +48,8 @@
           #"Date: [a-zA-Z0-9:, ]+ GMT"
           b
           #"Date: REDACTED GMT"))
-  (write-response (response/jsexpr (list "whoop" (hasheq 'there '("it" "is")))
-                                   #:seconds 0)
+  (write-response tm (response/jsexpr (list "whoop" (hasheq 'there '("it" "is")))
+                                      #:seconds 0)
                   #f))
  =>
  (bytes-sort
@@ -57,15 +57,15 @@
 
  ; The default MIME type ("application/json; charset=utf-8")
  ; can be overridden:
- (write-response (response/jsexpr ""
-                                  #:mime-type #"application/xml"))
+ (write-response tm (response/jsexpr ""
+                                     #:mime-type #"application/xml"))
  =>
  (bytes-sort
   #"HTTP/1.1 200 OK\r\nDate: REDACTED GMT\r\nLast-Modified: REDACTED GMT\r\nServer: Racket\r\nContent-Type: application/xml\r\n\r\n\"\"")
 
  ; Custom headers can be used:
- (write-response (response/jsexpr #t
-                                  #:headers (list (header #"head" #"bang"))))
+ (write-response tm (response/jsexpr #t
+                                     #:headers (list (header #"head" #"bang"))))
  =>
  (bytes-sort
   #"HTTP/1.1 200 OK\r\nDate: REDACTED GMT\r\nLast-Modified: REDACTED GMT\r\nServer: Racket\r\nContent-Type: text/html; charset=utf-8\r\nhead: bang\r\n\r\ntrue"))
