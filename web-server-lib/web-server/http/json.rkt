988184dced540a51b08b3d9953773a83022a2470
Jesse Alama <jesse@lisp.sh>
2019-07-29 07:02:25 +0200

    Define response/jsexpr

---
+++ web-server-lib/web-server/http/json.rkt
@@ -0,0 +1,40 @@
+#lang racket/base
+
+(require racket/list
+         racket/contract
+         json
+         (except-in net/cookies/server
+                    make-cookie)
+         (file "request-structs.rkt")
+         (file "response-structs.rkt")
+         (file "cookie.rkt")
+         (file "status-code.rkt"))
+
+(module+ test
+  (require rackunit))
+
+(define (response/jsexpr
+         jsexpr
+         #:code [code 200]
+         #:message [message #f]
+         #:seconds [seconds (current-seconds)]
+         #:mime-type [mime-type APPLICATION/JSON-MIME-TYPE]
+         #:cookies [cooks empty]
+         #:headers [hdrs empty])
+  (response
+   code (infer-response-message code message) seconds mime-type
+   ; rfc2109 also recommends some cache-control stuff here for cookies
+   (append hdrs (map cookie->header cooks))
+   (λ (out)
+     (write-json jsexpr out))))
+
+(provide/contract
+ [response/jsexpr
+  ((jsexpr?)
+   (#:code response-code/c
+    #:message (or/c #f bytes?)
+    #:seconds real?
+    #:mime-type (or/c #f bytes?)
+    #:cookies (listof cookie?)
+    #:headers (listof header?))
+   . ->* . response?)])
