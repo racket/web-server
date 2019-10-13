6daf36cb0efa46e829ee841fc2af1cbe65110692
Matthew Flatt <mflatt@racket-lang.org>
2019-10-13 08:26:04 -0600

    adjust test to allow variation in error text

--- web-server-test/tests/web-server/pr/12658.rkt
+++ web-server-test/tests/web-server/pr/12658.rkt
@@ -2,10 +2,9 @@
 (require rackunit
          "12658-mod.rkt")
 
-(check-equal?
- (with-handlers ([exn? (λ (x) (exn-message x))])
-   (go 42)
-   (error 'go "Failed to throw exn"))
- "go: arity mismatch;\n the expected number of arguments does not match the given number\n  expected: 0\n  given: 1\n  arguments...:\n   42")
+(check-exn
+ exn:fail:contract:arity?
+ (lambda () (go 42))
+ #rx"go: arity mismatch;\n the expected number of arguments does not match the given number\n  expected: 0\n  given: 1")
 
 
