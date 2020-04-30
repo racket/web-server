30613749dd1376c327e5a612df05f7a032982a73
Jay McCarthy <jay.mccarthy@gmail.com>
2020-04-30 08:12:55 -0400

    Remove timeout flakeyness

--- web-server-test/tests/web-server/dispatchers/dispatch-log-test.rkt
+++ web-server-test/tests/web-server/dispatchers/dispatch-log-test.rkt
@@ -1,5 +1,4 @@
 #lang racket/base
-
 (require rackunit
          (only-in mzlib/file
                   make-temporary-file)
@@ -13,8 +12,6 @@
          (prefix-in sequencer: web-server/dispatchers/dispatch-sequencer)
          "../util.rkt")
 
-
-
 (let ([bad-logger 'this-should-fail])
   (check-exn exn:fail:contract?
              (lambda ()
@@ -25,26 +22,27 @@
 (define (do-nothing-dispatcher conn req)
   'do-nothing)
 
-(let* ([op (open-output-string)]
-       [log-dispatcher (logger:make #:format 'apache-default
-                                    #:log-path op)]
-       [dispatcher (sequencer:make log-dispatcher
-                                   do-nothing-dispatcher)]
-       [req (request #"GET"
-                     (string->url "whatever")
-                     (list)
-                     (delay (list))
-                     #f
-                     "localhost"
-                     80
-                     "nada")]
-       [conn (fake-connection-for-bytes #"")])
+(let ()
+  (define-values (ip op) (make-pipe))
+  (define log-dispatcher
+    (logger:make #:format 'apache-default
+                 #:log-path op))
+  (define dispatcher
+    (sequencer:make log-dispatcher
+                    do-nothing-dispatcher))
+  (define req
+    (request #"GET"
+             (string->url "whatever")
+             (list)
+             (delay (list))
+             #f
+             "localhost"
+             80
+             "nada"))
+  (define conn (fake-connection-for-bytes #""))
   (check-equal? 'do-nothing (dispatcher conn req))
-
-  (sleep 2) ; flaky delay needed to ensure that the log
-            ; channel has a moment to process its input
-
-  ; the [...] part of the regexp matches a time-dependent
-  ; piece of the log data
-  (check-true (regexp-match-exact? #px"nada - - \\[[^\\]].+\\] \"GET whatever HTTP/1.1\" - -\n"
-                                   (bytes->string/utf-8 (get-output-bytes op)))))
+  (define log-entry (read-line ip))
+  ;; the [...] part of the regexp matches a time-dependent piece of
+  ;; the log data
+  (check-true (regexp-match-exact? #px"nada - - \\[[^\\]].+\\] \"GET whatever HTTP/1.1\" - -"
+                                   log-entry)))
