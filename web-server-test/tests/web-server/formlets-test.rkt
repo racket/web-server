6a655c1bbf5d9b89880f319c96b98d0a1f3ac6c3
Masaya Tojo <masaya@tojo.tokyo>
2023-03-11 17:59:49 +0900

    Update checkbox-group test to check #:wrap

--- web-server-test/tests/web-server/formlets-test.rkt
+++ web-server-test/tests/web-server/formlets-test.rkt
@@ -330,15 +330,16 @@
                    (test-display (checkbox-group 
                                   (list 1 2 3)
                                   #:display number->string 
+                                  #:wrap (λ (input display) (list display input))
                                   #:checked? even?
                                   #:attributes (λ (e) (list (list 'plus-one (number->string (add1 e)))))))
-                   '((input ((name "input_0") (type "checkbox") (value "0") (plus-one "2")))
-                     "1"
-                     (input ((name "input_0") (type "checkbox") (value "1") (checked "true") (plus-one "3")))
+                   '("1"
+                     (input ((name "input_0") (type "checkbox") (value "0") (plus-one "2")))
                      "2"
-                     (input ((name "input_0") (type "checkbox") (value "2") (plus-one "4")))
-                     "3"))
-      
+                     (input ((name "input_0") (type "checkbox") (value "1") (checked "true") (plus-one "3")))
+                     "3"
+                     (input ((name "input_0") (type "checkbox") (value "2") (plus-one "4")))))
+
       ; submit
       (test-equal? "submit"
                    (->cons (test-process (submit #"start") (list (make-binding:form #"input_0" #"value"))))
