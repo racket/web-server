3e0e0892caca2e0744cb722d8403c053a85418a1
Bogdan Popa <bogdan@defn.io>
2025-07-19 08:58:54 +0300

    test,serve: adjust kill test to pass in ci

--- web-server-test/tests/web-server/serve-tests.rkt
+++ web-server-test/tests/web-server/serve-tests.rkt
@@ -5,6 +5,7 @@
          racket/port
          racket/promise
          rackunit
+         version/utils
          web-server/http
          web-server/safety-limits
          web-server/servlet-dispatch
@@ -52,10 +53,10 @@
     (fail-check "timed out"))
   (check-true
    (real-time . >= . lo)
-   (format "took more than ~a seconds to run" lo))
+   (format "took less than ~a seconds to run" lo))
   (check-true
    (real-time . <= . hi)
-   (format "took less than ~a seconds to run" hi)))
+   (format "took more than ~a seconds to run" hi)))
 
 (define serve-tests
   (test-suite
@@ -71,72 +72,84 @@
        (lambda (_port stop)
          (check-duration stop 0 1 5))))
 
-    (test-case "waits for in-progress requests to finish"
-      (call-with-web-server
-       (lambda (_req)
-         (response/output
-          (lambda (out)
-            (for ([idx (in-range 2)])
-              (displayln idx out)
-              (sleep 1)))))
-       (lambda (port stop)
-         (define hc (http-conn-open "127.0.0.1" #:port port))
-         (define-values (status _headers in)
-           (http-conn-sendrecv! hc "/"))
-         (check-equal? status #"HTTP/1.1 200 OK")
-         (check-duration stop 2 6 6)
-         (check-equal? (port->bytes in) #"0\n1\n"))))
+    ;; On versions prior to [1], net/http-client writes to standard
+    ;; error when reading from the connection's input port goes wrong.
+    ;; This makes raco test fail in --drdr mode, so avoid running these
+    ;; tests on versions before 8.17.0.6.
+    ;;
+    ;; [1]: https://github.com/racket/racket/pull/5296
+    (when (version<=? "8.17.0.6" (version))
+      (test-case "waits for in-progress requests to finish"
+        (call-with-web-server
+         (lambda (_req)
+           (response/output
+            (lambda (out)
+              (for ([idx (in-range 2)])
+                (displayln idx out)
+                (sleep 1)))))
+         (lambda (port stop)
+           (define hc (http-conn-open "127.0.0.1" #:port port))
+           (define-values (status _headers in)
+             (http-conn-sendrecv! hc "/"))
+           (check-equal? status #"HTTP/1.1 200 OK")
+           (check-duration stop 2 6 6)
+           (check-equal? (port->bytes in) #"0\n1\n"))))
 
-    (test-case "stops when in-progress requests stop"
-      (call-with-web-server
-       (lambda (_req)
-         (response/output
-          (lambda (out)
-            (for ([idx (in-range 10)])
-              (displayln idx out)
-              (sleep 1)))))
-       (lambda (port stop)
-         (define hc (http-conn-open "127.0.0.1" #:port port))
-         (define-values (status _headers in)
-           (http-conn-sendrecv! hc "/"))
-         (check-equal? status #"HTTP/1.1 200 OK")
-         (thread
-          (lambda ()
-            (read-line in)
-            (close-input-port in)
-            (http-conn-close! hc)))
-         (check-duration stop 1 3 5))))
+      (test-case "stops when in-progress requests stop"
+        (call-with-web-server
+         (lambda (_req)
+           (response/output
+            (lambda (out)
+              (for ([idx (in-range 10)])
+                (displayln idx out)
+                (sleep 1)))))
+         (lambda (port stop)
+           (define hc (http-conn-open "127.0.0.1" #:port port))
+           (define-values (status _headers in)
+             (http-conn-sendrecv! hc "/"))
+           (check-equal? status #"HTTP/1.1 200 OK")
+           (thread
+            (lambda ()
+              (read-line in)
+              (close-input-port in)
+              (http-conn-close! hc)))
+           (check-duration stop 1 3 5))))
 
-    (test-case "kills the server if stop is called twice"
-      (define started?-sema
-        (make-semaphore))
-      (call-with-web-server
-       (lambda (_req)
-         (response/output
-          (lambda (out)
-            (displayln "start" out)
-            (semaphore-post started?-sema)
-            (sleep 100)
-            (displayln "end" out))))
-       (lambda (port stop)
-         (define hc (http-conn-open "127.0.0.1" #:port port))
-         (define-values (status _headers in)
-           (http-conn-sendrecv! hc "/"))
-         (check-equal? status #"HTTP/1.1 200 OK")
-         (define data-promise
-           (delay/thread
-            (port->bytes in)))
-         (semaphore-wait started?-sema)
-         (define stop-thds
-           (for/list ([_ (in-range 2)])
-             (thread stop)))
-         (check-duration
-          (lambda ()
-            (for-each thread-wait stop-thds))
-          0 1 2)
-         (check-equal?
-          (force data-promise)
-          #"start\n")))))))
+      (test-case "kills the server if stop is called twice"
+        (define started?-sema
+          (make-semaphore))
+        (call-with-web-server
+         (lambda (_req)
+           (response/output
+            (lambda (out)
+              (displayln "start" out)
+              (semaphore-post started?-sema)
+              (sleep 100)
+              (displayln "end" out))))
+         (lambda (port stop)
+           (define hc (http-conn-open "127.0.0.1" #:port port))
+           (define-values (status _headers in)
+             (http-conn-sendrecv! hc "/"))
+           (check-equal? status #"HTTP/1.1 200 OK")
+           (define data-promise
+             (delay/thread
+              (with-handlers ([(lambda (e)
+                                 (and (exn:fail? e)
+                                      (regexp-match? #rx"input port is closed" (exn-message e))))
+                               (lambda (_)
+                                 #"")])
+                (port->bytes in))))
+           (semaphore-wait started?-sema)
+           (define stop-thds
+             (for/list ([_ (in-range 2)])
+               (thread stop)))
+           (check-duration
+            (lambda ()
+              (for-each thread-wait stop-thds))
+            0 1 2)
+           (check-match
+            (force data-promise)
+            (or #"" #"start\n")))))))))
 
 (module+ test
   (require rackunit/text-ui)
