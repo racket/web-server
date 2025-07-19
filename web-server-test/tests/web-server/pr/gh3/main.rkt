90d4a6190016892a6c441534b893e86be3a03365
Bogdan Popa <bogdan@defn.io>
2025-07-19 15:08:51 +0300

    test,gh3: disable test for old versions of Racket
    Also, adjust the test to use serve to avoid needing to parse the
    output port to get the server's listening port.

--- web-server-test/tests/web-server/pr/gh3/main.rkt
+++ web-server-test/tests/web-server/pr/gh3/main.rkt
@@ -1,38 +1,45 @@
 #lang racket/base
-(require racket/runtime-path
-         racket/port
-         racket/list
-         web-server/servlet
-         net/http-client
-         web-server/servlet-env)
 
-(define-runtime-path here ".")
+(require net/http-client
+         racket/async-channel
+         racket/port
+         racket/runtime-path
+         version/utils
+         web-server/http
+         web-server/servlet-dispatch
+         web-server/web-server)
+
+(define-runtime-path private-key.pem
+  "private-key.pem")
+(define-runtime-path server-cert.pem
+  "server-cert.pem")
 
 (module+ test
   (require rackunit)
-  
-  (define-values (pipe-i pipe-o) (make-pipe))
 
-  (define server-t
-    (parameterize ([current-output-port pipe-o])
-      (thread
-       (λ ()
-         (serve/servlet (lambda (req) (response/xexpr `(html (body (h1 "Hello")))))
-                        #:launch-browser? #f
-                        #:port 0
-                        #:listen-ip #f
-                        #:ssl? #t
-                        #:ssl-cert (build-path here "server-cert.pem")
-                        #:ssl-key (build-path here "private-key.pem")
-                        #:servlet-regexp #rx"")))))
-
-  (define the-port
-    (string->number
-     (second
-      (regexp-match #rx"localhost:([0-9]+).$" (read-line pipe-i)))))
-  
-  (define-values (status headers body)
-    (http-sendrecv "localhost" "/" #:port the-port #:ssl? #t))
-  
-  (check-equal? status #"HTTP/1.1 200 OK") 
-  (check-equal? (port->bytes body) #"<html><body><h1>Hello</h1></body></html>"))
+  ;; Old versions of Racket don't work well with recent versions of
+  ;; OpenSSL found in CI. So, skip this test for older Rackets.
+  (unless (version<? (version) "8.16")
+    (define ssl-connect@
+      (make-ssl-connect@ server-cert.pem private-key.pem))
+    (define confirmation-ch
+      (make-async-channel))
+    (define stop
+      (serve
+       #:dispatch
+       (dispatch/servlet
+        (lambda (_req)
+          (response/xexpr `(html (body (h1 "Hello"))))))
+       #:port 0
+       #:confirmation-channel confirmation-ch
+       #:dispatch-server-connect@ ssl-connect@))
+    (define port
+      (sync confirmation-ch))
+    (when (exn:fail? port)
+      (raise port))
+    (test-case "regression test for GH issue #3"
+      (define-values (status _headers body)
+        (http-sendrecv "localhost" "/" #:port port #:ssl? #t))
+      (check-equal? status #"HTTP/1.1 200 OK")
+      (check-equal? (port->bytes body) #"<html><body><h1>Hello</h1></body></html>"))
+    (stop)))
